481fb93e3148e26e5bff22c980510dd191e7cf23
Natalia <taliakolmakova@gmail.com>
2017-12-14 17:14:49 +0200

    added add test into album.service.spec.js

--- src/tests/album.service.spec.js
+++ src/tests/album.service.spec.js
@@ -3,23 +3,6 @@
 describe('Album', function () {
 	var $httpBackend;
 	var Album;
-	var albumsAll = [
-		{
-			"title": "Empire Burlesque",
-			"artist": "Bob Dylan",
-			"country": "USA",
-			"company": "Columbia",
-			"price": 10.90,
-			"year": 1985
-		}, {
-			"title": "Hide your heart",
-			"artist": "Bonnie Tyler",
-			"country": "UK",
-			"company": "CBS Records",
-			"price": 9.90,
-			"year": 1988
-		}
-	];
 
 	// Add a custom equality tester before each test
 	beforeEach(function () {
@@ -42,6 +25,24 @@ describe('Album', function () {
 	});
 
 	it('should fetch the all albums from `/albums/all`', function () {
+		var albumsAll = [
+			{
+				"title": "Empire Burlesque",
+				"artist": "Bob Dylan",
+				"country": "USA",
+				"company": "Columbia",
+				"price": 10.90,
+				"year": 1985
+			}, {
+				"title": "Hide your heart",
+				"artist": "Bonnie Tyler",
+				"country": "UK",
+				"company": "CBS Records",
+				"price": 9.90,
+				"year": 1988
+			}
+		];
+
 		$httpBackend.expectGET('/albums/all').respond(albumsAll);
 
 		var albums = Album.all();
@@ -80,7 +81,49 @@ describe('Album', function () {
 		});
 
 		$httpBackend.flush();
+	});
+	
+	it('should add an album to the album list', function () {
+		var albumToAdd = {
+			title: "Empire Burlesque",
+			artist: "Bob Dylan",
+			country: "USA",
+			company: "Columbia",
+			price: 10.90,
+			year: 1985
+		};
 
+		var albumToRespond = {
+			title: "Empire Burlesque",
+			artist: "Bob Dylan",
+			country: "USA",
+			company: "Columbia",
+			price: 10.90,
+			year: 1985,
+			id: 90
+		};
+
+		$httpBackend.expectPOST('/albums/add', albumToAdd).respond(200, albumToRespond);
+		var album = new Album(albumToAdd);
+		album.$add({}, function (addedAlbum) {
+			expect(addedAlbum).toEqual(albumToRespond);
+		});
+
+		$httpBackend.flush();
 	})
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
